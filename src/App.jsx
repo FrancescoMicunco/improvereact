0df3262b6088616fdb134bfb8299878c9e9ef8ae
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-11-21 14:44:10 +0100

    structure with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Navbar from './Component/Navbar';
-import Home from './Component/Home';
-import Contact from './Component/Contact';
-import Dishes from './Component/Dishes';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Home from "./Component/Home";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Contact from "./Component/Contact";
+import Dishes from "./Component/Dishes";
+import MyNavbar from "./Component/MyNavbar";
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <BrowserRouter>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/Dishes" element={<Dishes />} />
-            </Routes>
-          </BrowserRouter>
-        </header>{" "}
-      </div>
-    );
+  return (
+    <div className="App">
+      {/* <header className="App-header"> */}
+      <BrowserRouter>
+<MyNavbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/Dishes" element={<Dishes />} />
+        </Routes>
+      </BrowserRouter>
+      {/* </header>{" "} */}
+    </div>
+  );
 }
 
 export default App;
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,3 +1,4 @@
+
 const Home =()=>{
 return <h1>This is the home</h1>
 }
